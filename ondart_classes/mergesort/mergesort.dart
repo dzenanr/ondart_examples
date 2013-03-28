57c46b1fb2fab81b4f3155300682237c44e439c5
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-03-28 13:37:15 -0400

    update merge sort

--- ondart_classes/mergesort/mergesort.dart
+++ ondart_classes/mergesort/mergesort.dart
@@ -5,54 +5,54 @@ import 'package:unittest/unittest.dart';
 class MergeSorter {
   int length;
   List unsorted ;
-  List placeholder;
+  List _placeholder;
 
   List sort(List values) {
     this.unsorted = values;
     length = values.length.toInt();
-    placeholder = new List(length);
-    mergeSort(0, length-1);
+    _placeholder = new List(length);
+    _mergeSort(0, length-1);
     return unsorted;
   }
 
-  void mergeSort(low,high) {
+  void _mergeSort(num low, num high) {
     if(low < high) {
       var middle = ((low+high)/2).toInt();
-      mergeSort(low,middle);
-      mergeSort((middle+1), high);
-      merge(low,middle,high);
+      _mergeSort(low,middle);
+      _mergeSort((middle+1), high);
+      _merge(low,middle,high);
     }
   }
 
-  void merge(int low, int middle, int high) {
+  void _merge(num low, num middle, num high) {
     for (int i = low; i <= high; i++){
-      placeholder[i] = unsorted[i];
+      _placeholder[i] = unsorted[i];
     }
 
-    int i = low;
-    int j = middle+1;
-    int k = low;
+    num i = low;
+    num j = middle + 1;
+    num k = low;
 
     while (i <= middle && j <= high) {
-      if (placeholder[i] <= placeholder[j]) {
-        unsorted[k] = placeholder[i];
+      if (_placeholder[i] <= _placeholder[j]) {
+        unsorted[k] = _placeholder[i];
         i ++;
       } else {
-        unsorted[k] = placeholder[j];
+        unsorted[k] = _placeholder[j];
         j++;
       }
       k++;
     }
 
     while (i <= middle) {
-      unsorted[k] = placeholder[i];
+      unsorted[k] = _placeholder[i];
       k++;
       i++;
     }
   }
 }
 
-void main(){
+void main() {
   var sorter = new MergeSorter();
   test('Merge_Worst', () =>
       expect(sorter.sort([5,4,3,2,1]), orderedEquals([1,2,3,4,5])));
