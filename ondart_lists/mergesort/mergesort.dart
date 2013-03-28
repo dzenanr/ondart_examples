61c131b26020ebf8ddc000e5354823efc79119c8
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-03-28 13:00:37 -0400

    update merge sort

--- ondart_lists/mergesort/mergesort.dart
+++ ondart_lists/mergesort/mergesort.dart
@@ -1,64 +1,59 @@
 import 'package:unittest/unittest.dart';
 
-// from http://chmcguir.wordpress.com/2012/11/09/dart-mergesort/
+// adapted from http://chmcguir.wordpress.com/2012/11/09/dart-mergesort/
 
-class Sorting {
-  var unsorted ;
-  List placeholder;
+List unsorted ;
+List placeholder;
 
-  var length;
+List sort(List values) {
+  unsorted = values;
+  var length = values.length.toInt();
+  placeholder = new List(length);
+  mergeSort(0, length-1);
+  return unsorted;
+}
 
-  List sort(List values) {
-    this.unsorted = values;
-    length = values.length.toInt();
-    placeholder = new List(length);
-    mergeSort(0, length-1);
-    return unsorted;
+void mergeSort(low,high) {
+  if(low < high) {
+    var middle = ((low+high)/2).toInt();
+    mergeSort(low,middle);
+    mergeSort((middle+1), high);
+    merge(low,middle,high);
+  }
+}
+
+void merge(int low, int middle, int high) {
+  for (int i = low; i <= high; i++){
+    placeholder[i] = unsorted[i];
   }
 
-  void mergeSort(low,high) {
-    if(low < high) {
-      var middle = ((low+high)/2).toInt();
-      mergeSort(low,middle);
-      mergeSort((middle+1), high);
-      merge(low,middle,high);
-    }
-  }
+  int i = low;
+  int j = middle+1;
+  int k = low;
 
-  void merge(int low, int middle, int high) {
-    for (int i = low; i <= high; i++){
-      placeholder[i] = unsorted[i];
-    }
-
-    int i = low;
-    int j = middle+1;
-    int k = low;
-
-    while (i <= middle && j <= high) {
-      if (placeholder[i] <= placeholder[j]) {
-        unsorted[k] = placeholder[i];
-        i ++;
-      } else {
-        unsorted[k] = placeholder[j];
-        j++;
-      }
-      k++;
-    }
-
-    while (i <= middle) {
+  while (i <= middle && j <= high) {
+    if (placeholder[i] <= placeholder[j]) {
       unsorted[k] = placeholder[i];
-      k++;
-      i++;
+      i ++;
+    } else {
+      unsorted[k] = placeholder[j];
+      j++;
     }
+    k++;
+  }
+
+  while (i <= middle) {
+    unsorted[k] = placeholder[i];
+    k++;
+    i++;
   }
 }
 
 void main(){
-  Sorting sorter = new Sorting();
   test('Merge_Worst', () =>
-      expect(sorter.sort([5,4,3,2,1]), orderedEquals([1,2,3,4,5])));
+      expect(sort([5,4,3,2,1]), orderedEquals([1,2,3,4,5])));
   test('Merge_Best', () =>
-      expect(sorter.sort([1,2,3,4,5]), orderedEquals([1,2,3,4,5])));
+      expect(sort([1,2,3,4,5]), orderedEquals([1,2,3,4,5])));
 }
 
 
