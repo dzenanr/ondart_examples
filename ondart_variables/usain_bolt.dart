1271e3cc3e2855d71792e8ce825eca7d28e3771e
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-11-09 10:25:06 -0500

    avoid warnings by using comments

--- ondart_variables/usain_bolt.dart
+++ ondart_variables/usain_bolt.dart
@@ -2,6 +2,7 @@ main() {
   var bolt = 9.58;
   print(bolt);
   print(bolt is num);
-  //print(bolt is double);
+  // When compiled to JS, this test might return true when the left hand side is an int
+  // print(bolt is double);
 }
 
